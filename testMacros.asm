f629e1e1d9f5e42d8c182d8ec354af16c10919bb
SnarkyDeveloper <TheCoderKid@proton.me>
2025-08-21 14:57:33 -0700

    TODO: Add dynamic classes and inheritance, rewrote current class code to be easier to implement

--- testMacros.asm
+++ testMacros.asm
@@ -1,19 +1,19 @@
 %include "map.inc"
 %include "macros.inc"
 new string, outside, "This variable is defined outside any section", newline 
-new string, example, "print ", doublequote, "yay newlines with escapes \\n", DOUBLEQUOTE
+new string, example, "print ", doublequote, "yay newlines with escapes \\n", doublequote
 
 
 class cats
     value cats.calico, "A calico cat is a domestic cat with a coat that is typically three colors: white, black, and orange.\n"
-    method cat, grey
+    method cats, grey, static
         print cats.calico
     end
 
 func main():
     print "Code ran: \n", example, newline, newline
 
-    call cat.grey
+    run cats, grey
     print "Called cat.grey function CLASSES YIPPIEEE\n"
     print "Output: \n"
     print "yay newlines with escapes \n"
